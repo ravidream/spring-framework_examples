caf7a77f8785e4a619bb060a75738346eecfd08d
RaviTh <ravi.thapa@csireg.com>
2018-06-21 13:38:52 -0500

    Spring Hello world example using ClassPathXmlApplicationContext

---
+++ SpringEx2ApplicationContext/src/com/spring/test/Test.java
@@ -0,0 +1,24 @@
+package com.spring.test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+/**
+ * 
+ * @author Ravi Thapa
+ *
+ */
+public class Test {
+public static void main(String[] args) {
+//	Resource resource=new ClassPathResource("applicationContext.xml");
+//	BeanFactory factory=new XmlBeanFactory(resource);
+	ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+	Student student=(Student)context.getBean("studentBean");
+	student.displayInfo();
+}
+}
