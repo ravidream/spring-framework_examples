cb9970df93e028c9551d2cedff33e0db99f6fbd2
RaviTh <ravi.thapa@csireg.com>
2018-06-21 14:04:56 -0500

    Spring example for Dependency Injection using setter

---
+++ SpringEx3DISetterInjection/src/com/spring/test/Test.java
@@ -0,0 +1,21 @@
+package com.spring.test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.*;
+
+/**
+ * 
+ * @author Ravi Thapa
+ *
+ */
+public class Test {
+	public static void main(String[] args) {
+
+		Resource resource = new ClassPathResource("applicationContext.xml");
+		BeanFactory factory = new XmlBeanFactory(resource);
+
+		Employee e = (Employee) factory.getBean("empBean");
+		e.display();
+	}
+}
